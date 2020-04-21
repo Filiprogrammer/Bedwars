b6b2d30b9fd40761db8d9543a4b388b0a79b9650
Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>
2020-04-21 22:36:12 +0200

    Sixty-eighth commit

--- src/filip/bedwars/sign/GameJoinSign.java
+++ src/filip/bedwars/sign/GameJoinSign.java
@@ -4,6 +4,8 @@ import org.bukkit.Location;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 
+import filip.bedwars.config.MainConfig;
+
 public class GameJoinSign {
 
 	Location location;
@@ -12,6 +14,16 @@ public class GameJoinSign {
 	public GameJoinSign(Location location, String mapName) {
 		this.location = location;
 		this.mapName = mapName;
+		
+		Sign sign = getSign();
+		
+		if (sign != null) {
+			sign.setLine(0, MainConfig.getInstance().getJoinSignLine(0).replace("%arenaname%", mapName));
+			sign.setLine(1, MainConfig.getInstance().getJoinSignLine(1).replace("%arenaname%", mapName));
+			sign.setLine(2, MainConfig.getInstance().getJoinSignLine(2).replace("%arenaname%", mapName));
+			sign.setLine(3, MainConfig.getInstance().getJoinSignLine(3).replace("%arenaname%", mapName));
+			sign.update();
+		}
 	}
 	
 	public Sign getSign() {
