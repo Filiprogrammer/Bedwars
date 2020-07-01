ea3df25f4cdcb834685a4d90285eda2a9e3ed4fc
Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>
2020-07-01 17:39:51 +0200

    Hundred-first commit

--- src/filip/bedwars/game/shop/BedRestoreTeamShopReward.java
+++ src/filip/bedwars/game/shop/BedRestoreTeamShopReward.java
@@ -9,6 +9,8 @@ import filip.bedwars.game.GamePlayer;
 import filip.bedwars.game.Team;
 import filip.bedwars.game.Team.TeamUpgradeType;
 import filip.bedwars.inventory.ItemBuilder;
+import filip.bedwars.utils.MessageSender;
+import filip.bedwars.utils.SoundPlayer;
 import filip.bedwars.utils.TeamColorConverter;
 
 public class BedRestoreTeamShopReward extends TeamShopReward {
@@ -48,7 +50,24 @@ public class BedRestoreTeamShopReward extends TeamShopReward {
 			p.sendTitle(MessagesConfig.getInstance().getStringValue(p.getLocale(), "your-bed-restored"), "", 10, 70, 20);
 		}
 		
+		broadcastBedRestored(team);
+		
 		return true;
+	}
+	
+	private void broadcastBedRestored(Team team) {
+		if (team.getMembers().size() == 0)
+			return;
+		
+		for (Player p : team.getMembers().get(0).getPlayer().getWorld().getPlayers()) {
+			String colorStr = TeamColorConverter.convertTeamColorToStringForMessages(team.getBase().getTeamColor(), p.getLocale());
+			
+			MessageSender.sendMessage(p,
+					MessagesConfig.getInstance().getStringValue(p.getLocale(), "bed-restored")
+					.replace("%teamcolor%", colorStr));
+			
+			SoundPlayer.playSound("bed-restored", p);
+		}
 	}
 
 }
