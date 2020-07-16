2f72b5e1a9572a7eb133ac4e72fb990277429e57
Marcel Fürpaß <marcel.fuerpass@edu.szu.at>
2020-07-16 16:20:45 +0200

    Update team upgrade design

--- src/filip/bedwars/game/shop/HealPoolTeamShopReward.java
+++ src/filip/bedwars/game/shop/HealPoolTeamShopReward.java
@@ -1,5 +1,7 @@
 package filip.bedwars.game.shop;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -16,15 +18,26 @@ public class HealPoolTeamShopReward extends TeamShopReward {
 	@Override
 	public ItemStack getDisplayItem(Team team) {
 		int level = team.upgrades.get(type);
+		StringBuilder lore = new StringBuilder();
+		
+		int i;
+		for (i = 0; i < level; ++i)
+			lore.append("§l§a[]");
+		
+		for (; i < maxLevel; ++i)
+			lore.append("§l§7[]");
+		
+		@SuppressWarnings("serial")
+		ItemBuilder itemBuilder = new ItemBuilder().setLore(new ArrayList<String>() {{ add(lore.toString()); }});
 		
 		// TODO: Read the messages from a config file
 		if (level >= maxLevel) {
-			return new ItemBuilder()
+			return itemBuilder
 					.setMaterial(Material.ENCHANTED_GOLDEN_APPLE)
 					.setName("§rHeal Pool is maxed out")
 					.build();
 		} else {
-			return new ItemBuilder()
+			return itemBuilder
 					.setMaterial(Material.GOLDEN_APPLE)
 					.setName("§rBuy Heal Pool Level " + (team.upgrades.get(type) + 1))
 					.build();
--- src/filip/bedwars/game/shop/MiningBoostTeamShopReward.java
+++ src/filip/bedwars/game/shop/MiningBoostTeamShopReward.java
@@ -1,5 +1,7 @@
 package filip.bedwars.game.shop;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -16,15 +18,26 @@ public class MiningBoostTeamShopReward extends TeamShopReward {
 	@Override
 	public ItemStack getDisplayItem(Team team) {
 		int level = team.upgrades.get(type);
+		StringBuilder lore = new StringBuilder();
+		
+		int i;
+		for (i = 0; i < level; ++i)
+			lore.append("§l§a[]");
+		
+		for (; i < maxLevel; ++i)
+			lore.append("§l§7[]");
+		
+		@SuppressWarnings("serial")
+		ItemBuilder itemBuilder = new ItemBuilder().setLore(new ArrayList<String>() {{ add(lore.toString()); }});
 		
 		// TODO: Read the messages from a config file
 		if (level >= maxLevel) {
-			return new ItemBuilder()
+			return itemBuilder
 					.setMaterial(Material.DIAMOND_PICKAXE)
 					.setName("§rMining Boost is maxed out")
 					.build();
 		} else {
-			return new ItemBuilder()
+			return itemBuilder
 					.setMaterial(Material.GOLDEN_PICKAXE)
 					.setName("§rBuy Mining Boost Level " + (team.upgrades.get(type) + 1))
 					.build();
